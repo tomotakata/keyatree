bbd779c05ce38adfca1eb6a3d493a23d185db625
tomotakata <otomo.palco.me@gmail.com>
2026-06-02 13:46:56 +0900

    feat: add tracked doc page for floorplan requirements

---
+++ app/docs/floorplan-requirements/page.tsx
@@ -0,0 +1,16 @@
+import TrackedDocPage from "@/components/docs/TrackedDocPage";
+
+export default function FloorplanRequirementsPage() {
+  return (
+    <TrackedDocPage
+      docId="floorplan-requirements-v1"
+      docTitle="賃貸物件 間取り作成機能 要件定義書"
+      docSubtitle="賃貸物件 間取り作成機能 v1.0"
+      docVersion="v1.0"
+      docDate="2025年6月"
+      docTarget="物件管理 / 間取り作成機能 要件定義"
+      docAuthor="開発チーム"
+      iframeSrc="/docs/floorplan-requirements.html"
+    />
+  );
+}
